6d32762620a9244f39145f6cdaf568ac58e72318
Giorgio Silvestris <giorgio.silvestris@gmail.com>
2019-11-20 18:40:14 +0100

    update

--- src/test/java/org/dew/cades/TestCAdES.java
+++ src/test/java/org/dew/cades/TestCAdES.java
@@ -7,35 +7,35 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 public class TestCAdES extends TestCase {
-	
-	public TestCAdES(String testName) {
-		super(testName);
-	}
-	
-	public static Test suite() {
-		return new TestSuite(TestCAdES.class);
-	}
-	
-	public 
-	void testApp() 
-		throws Exception 
-	{
-		String library = "";
-		// String library = "D:\\Main\\bit4xpki.dll";
-		String pin     = "31080808";
-		
-		CAdESSigner cades = new CAdESSigner(library, pin);
-		
-		String filePath = System.getProperty("user.home") + File.separator + "test_cades.txt";
-		
-		File file = new File(filePath);
-		if(!file.exists()) {
-			cades.writeFile(filePath, "test".getBytes());
-		}
-		System.out.println("File to sign: " + filePath);
-		
-		String signedFile = cades.sign(filePath);
-		System.out.println("Signed file: " + signedFile);
-	}
-	
+  
+  public TestCAdES(String testName) {
+    super(testName);
+  }
+  
+  public static Test suite() {
+    return new TestSuite(TestCAdES.class);
+  }
+  
+  public 
+  void testApp() 
+    throws Exception 
+  {
+    String library = "";
+    // String library = "D:\\Main\\bit4xpki.dll";
+    String pin     = "31080808";
+    
+    CAdESSigner cades = new CAdESSigner(library, pin);
+    
+    String filePath = System.getProperty("user.home") + File.separator + "test_cades.txt";
+    
+    File file = new File(filePath);
+    if(!file.exists()) {
+      cades.writeFile(filePath, "test".getBytes());
+    }
+    System.out.println("File to sign: " + filePath);
+    
+    String signedFile = cades.sign(filePath);
+    System.out.println("Signed file: " + signedFile);
+  }
+  
 }
